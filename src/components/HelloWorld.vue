0a09ced53364a9e60a3fefb2133c340fe3b0bedf
zhangsheng <2402536045@qq.com>
2022-06-19 11:09:49 +0800

    fist commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Test from './test.tsx'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <Test/>
+</template>
+
+
+<style scoped>
+
+</style>
