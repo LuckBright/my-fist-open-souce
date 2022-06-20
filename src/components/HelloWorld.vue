4e02a6abc5803b219800bb01a805dfc52c1da7a6
zhangsheng <2402536045@qq.com>
2022-06-20 15:31:07 +0800

    feat：😈TSX组件的运用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,21 @@
+<!--
+ * @Author: zhangsheng@youlu.com
+ * @Date: 2022-06-20 10:49:24
+ * @Desc: Do not edit
+ * @LastEditTime: 2022-06-20 15:27:15
+ * @LastEditors: zhangsheng@youlu.com
+-->
 <script setup lang="ts">
 import { ref } from 'vue'
+import Test from './test.tsx'
 </script>
 
 <template>
   <div class="text-3xl font-bold">随便写一个</div>
+  <Test>
+    我是通过slot渲染的
+    <template #title>
+      <h2 class="font-bold">我是通过title 插槽渲染的</h2>
+    </template>
+  </Test>
 </template>
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,1 +1,45 @@
-export default () => <div>test</div>
+/*
+ * @Author: zhangsheng@youlu.com
+ * @Date: 2022-06-20 10:49:24
+ * @Desc: Do not edit
+ * @LastEditTime: 2022-06-20 15:26:26
+ * @LastEditors: zhangsheng@youlu.com
+ */
+import { defineComponent, ref, withModifiers } from 'vue'
+import inputDemo from './InputDemo.vue'
+
+export default defineComponent({
+  directives: {
+    focus: {
+      mounted(el) {
+        el.focus()
+      }
+    }
+  },
+  components: {
+    inputDemo
+  },
+  setup(props, { slots }) {
+    const count = ref(0)
+
+    const inc = () => {
+      count.value++
+    }
+
+    const list = ref<string[]>(['a', 'b', 'c'])
+    return () => {
+      return (
+        <div onClick={withModifiers(inc, ['self'])}>
+          test：{count.value}
+          <inputDemo></inputDemo>
+          {list.value.map(str => (
+            <div>{str}</div>
+          ))}
+          <div>{slots.default ? slots.default() : 'not default content'}</div>
+          <div>{slots.title ? slots.title() : ''}</div>
+          <input type="text" v-focus v-model={count.value} />
+        </div>
+      )
+    }
+  }
+})
